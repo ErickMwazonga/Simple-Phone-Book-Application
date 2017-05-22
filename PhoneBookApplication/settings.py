9b2476a0cdb21d311c9b99838b8be9be8abcf9c6
Erick <erickmwazonga@gmail.com>
2017-05-22 11:52:10 +0300

    adding conf for Heroku deployment

--- PhoneBookApplication/settings.py
+++ PhoneBookApplication/settings.py
@@ -123,6 +123,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+# Heroku
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- PhoneBookApplication/wsgi.py
+++ PhoneBookApplication/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PhoneBookApplication.settings")
+#
+# application = get_wsgi_application()
+#
+
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PhoneBookApplication.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
