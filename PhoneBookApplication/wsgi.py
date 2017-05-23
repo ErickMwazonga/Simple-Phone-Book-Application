840f0401d9ac146beeb17541255922279010d2d4
Erick <erickmwazonga@gmail.com>
2017-05-23 10:09:19 +0300

    adding runtime.txt

--- PhoneBookApplication/wsgi.py
+++ PhoneBookApplication/wsgi.py
@@ -20,7 +20,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phonebook.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
