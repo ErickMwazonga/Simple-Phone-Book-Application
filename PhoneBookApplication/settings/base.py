21456e9fb5b79f1b4be2ca62b68e9f3b891ac173
Erick <erickmwazonga@gmail.com>
2017-05-23 11:48:30 +0300

    load data

--- PhoneBookApplication/settings/base.py
+++ PhoneBookApplication/settings/base.py
@@ -27,7 +27,7 @@ SECRET_KEY = '6yl@uw7!+z#egp80_zek433lp)iurt-zklm0=_!(^h@c_zgupg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- PhoneBookApplication/settings/production.py
+++ PhoneBookApplication/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 from .base import DATABASES
 
 DEBUG = True  # Why persecute me heroku???
-ALLOWED_HOSTS = ['app-phonebook.herokuapp.com', ]
+ALLOWED_HOSTS = ['app-phonebook.herokuapp.com', '127.0.0.1' ]
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
