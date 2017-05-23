afedbed4fe45e118e1a38b48b5159696596c16fe
Erick <erickmwazonga@gmail.com>
2017-05-23 11:05:15 +0300

    editing settings

--- PhoneBookApplication/settings/base.py
+++ PhoneBookApplication/settings/base.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 ASSETS_ROOT = 'phonebook/static'
